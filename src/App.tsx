9dc75d6c78a59583719b26577e277167d776ad79
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-11-02 21:49:05 +0700

    Add typing test functionality to App component
    Introduces typing test logic with random words, input handling, and word progression. Uses hooks for state management and keyboard event handling, and updates UI to display current input and active word.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,72 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
+import { RandomWords } from "./util/random-words.ts";
 
 function App() {
+  const [inputWord, setInputWord] = useState<string>("");
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [completedWords, setCompletedWords] = useState<string[]>([]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const randomWords = useMemo(() => RandomWords.getRandomWords(30), []);
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      inputRef.current?.focus();
+      if (e.key === " " || e.key === "Tab") {
+        e.preventDefault();
+        const currentWord = randomWords[currentWordIndex];
+        const isCorrect = inputWord.trim() === currentWord;
+
+        if (isCorrect) {
+          setCurrentWordIndex((prev) => prev + 1);
+          setInputWord("");
+          console.log(
+            "correct! now onto the word: ",
+            randomWords[currentWordIndex + 1],
+          );
+        }
+      }
+    },
+    [currentWordIndex, inputWord, randomWords],
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [handleKeyDown]);
+
   return (
-    <div>
-      <p>typing test</p>
+    <div className="flex flex-col p-5 min-h-screen bg-gray-100">
+      <div className="relative text-xl h-fit ">
+        <input
+          id="input"
+          type="text"
+          ref={inputRef}
+          value={inputWord}
+          onChange={(e) => setInputWord(e.target.value)}
+          className="opacity-0"
+        />
+        <div className="absolute">{inputWord}</div>
+        <div id="letter" className="flex flex-wrap">
+          {randomWords
+            .join(" ")
+            .split(" ")
+            .map((word, i) => {
+              return (
+                <div
+                  key={i}
+                  className={i == currentWordIndex ? "active" : ""}
+                  data-wordindex={i}
+                >
+                  {word.split("").map((letter, j) => {
+                    return <span key={j}>{letter}</span>;
+                  })}
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
