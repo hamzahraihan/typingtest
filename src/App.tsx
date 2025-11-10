dc6569bd1735f76b1bcd57032f4b945703bd554d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-11-10 14:23:56 +0700

    Refactor App layout and remove input logic
    Simplifies the App component by removing inputWord state and related input field. Updates layout to use a centered section with header, TypingTest, and footer elements for improved structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
 import { useRef } from "react";
 import "./App.css";
 import { TypingTest } from "./components/typing-test.tsx";
-import { useWordContext } from "./context/word-store-context.ts";
 
 function App() {
-  const inputWord = useWordContext((state) => state.inputWord);
-  const setInputWord = useWordContext((state) => state.setInputWord);
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <div className="antialiased flex flex-col p-5 min-h-screen bg-gray-100">
-      <div className="relative text-xl h-fit ">
-        <input
-          id="input"
-          type="text"
-          ref={inputRef}
-          value={inputWord}
-          onChange={(e) => setInputWord(e.target.value)}
-          className="opacity-0"
-          minLength={0}
-          maxLength={20}
-        />
-        <div className="absolute hidden">{inputWord}</div>
-        <TypingTest inputRef={inputRef} />
+    <section className="flex justify-center  bg-gray-100">
+      <div className="antialiased grid grid-row-6 p-5 min-h-screen max-w-screen-lg">
+        <div>header</div>
+        <div className="border border-red-300 row-span-6 self-center">
+          <TypingTest inputRef={inputRef} />
+        </div>
+        <footer>footer</footer>
       </div>
-    </div>
+    </section>
   );
 }
 
