4df1a50fef32c6c2e617678afd5755e59919414c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-11-17 14:42:11 +0700

    Update timer.ts

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,1 +1,23 @@
+export class Timer {
+  state: "PLAY" | "IDLE" | "FINISHED";
+  startTime: number | null = null;
+  endTime: number | null = null;
 
+  constructor(initialState: "PLAY" | "IDLE" | "FINISHED" = "IDLE") {
+    this.state = initialState;
+  }
+
+  public start() {
+    if (this.state === "IDLE") {
+      this.startTime = performance.now();
+      this.state = "PLAY";
+    }
+  }
+
+  public stop() {
+    if (this.state === "PLAY") {
+      this.endTime = performance.now();
+      this.state = "FINISHED";
+    }
+  }
+}
