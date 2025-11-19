6988dc6e1a26658cf802ca201e2ed1b24d8a7fc5
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-11-19 16:29:54 +0700

    Display WPM in WordStatus component
    Added WPM (words per minute) display to the WordStatus component by using the useWordContext hook. This provides users with real-time feedback on their typing speed.

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,3 +1,5 @@
+import { useWordContext } from "../context/word-store-context.ts";
+
 function WordStatus({
   randomWordsLength,
   typedWordsLength,
@@ -7,13 +9,17 @@ function WordStatus({
   typedWordsLength: number;
   className?: string;
 }) {
+  const wpm = useWordContext((state) => state.wpm);
   return (
-    <div
-      className={
-        "px-1 text-3xl text-gray-600 duration-200 transition-all " + className
-      }
-    >
-      {typedWordsLength}/{randomWordsLength}
+    <div>
+      <div
+        className={
+          "px-1 text-3xl text-gray-600 duration-200 transition-all " + className
+        }
+      >
+        {typedWordsLength}/{randomWordsLength}
+      </div>
+      <div>WPM: {wpm.toFixed(0)}</div>
     </div>
   );
 }
