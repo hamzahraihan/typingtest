e1aa23490b781e456ede18f83faa978a17d96487
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-11-19 16:29:49 +0700

    Remove border from TypingTest container
    Eliminated the red border from the div wrapping the TypingTest component to improve UI appearance.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <section className="flex justify-center  bg-gray-100">
       <div className="antialiased grid grid-row-6 p-5 min-h-screen max-w-screen-lg">
         <div>header</div>
-        <div className="border border-red-300 row-span-6 self-center">
+        <div className="row-span-6 self-center">
           <TypingTest inputRef={inputRef} />
         </div>
         <footer>footer</footer>
