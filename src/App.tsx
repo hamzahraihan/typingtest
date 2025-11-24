7f08a73fb339e22eec9d7b5bac7ccc3a19d8dd7a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-11-24 09:47:25 +0700

    Add difficulty selection to App component
    Introduces a set of difficulty buttons in the App header, allowing users to change the typing test difficulty using the word context. Updates layout and integrates difficulty state management.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { useRef } from "react";
 import "./App.css";
 import { TypingTest } from "./components/typing-test.tsx";
+import { useWordContext } from "./context/word-store-context.ts";
+import { difficulties, type Difficulty } from "./utils/difficulty.ts";
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const { setDifficulty } = useWordContext((state) => state);
 
   return (
     <section className="flex justify-center  bg-gray-100">
-      <div className="antialiased grid grid-row-6 p-5 min-h-screen max-w-screen-lg">
-        <div>header</div>
-        <div className="row-span-6 self-center">
+      <div className="grid grid-rows-6 p-5 min-h-screen max-w-screen-lg">
+        <div className="flex items-end">
+          <div className="flex gap-2 items-start mt-auto">
+            {difficulties.map((diff, i) => (
+              <button
+                key={diff + "-" + i}
+                className="border rounded-md border-gray-50 bg-gray-200 p-1"
+                onClick={() => setDifficulty(diff as Difficulty)}
+              >
+                {diff}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="row-span-4">
           <TypingTest inputRef={inputRef} />
         </div>
         <footer>footer</footer>
