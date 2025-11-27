b72a768d21009ebb58827180b620adc0e4d4da47
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-11-27 14:07:50 +0700

    Update word.tsx

--- src/components/word.tsx
+++ src/components/word.tsx
@@ -36,10 +36,6 @@ export const Word = forwardRef<HTMLDivElement, Props>(function Word(
     <div className={"scroll-mt-9 m-1 text-3xl " + wordChecker()} ref={ref}>
       {word.split("").map((letter, j) => {
         let className = "";
-        // if user input a correct letter, the color change to black
-        if (isCurrent && inputWord[j] === letter) {
-          className = "text-black";
-        }
 
         // if user input an incorrect letter, the color change to red
         if (isCurrent && inputWord[j] != letter) {
@@ -48,13 +44,15 @@ export const Word = forwardRef<HTMLDivElement, Props>(function Word(
 
         // if user not input a letter based from the current word, the color became transparent
         if (isCurrent && inputWord[j] == undefined) {
-          className = "text-gray-400";
+          className = "text-opacity-40";
         }
 
         // if user already typed a word, the color will change based on correctness
         if (!isCurrent && typedWords[wordIndex]) {
           className =
-            typedWords[wordIndex][j] === letter ? "text-black" : "text-red-500";
+            typedWords[wordIndex][j] === letter
+              ? "text-[#cad3f0] "
+              : "text-red-500";
         }
 
         // the currentWordIndex will be increase after user pressed tab or space button
@@ -62,14 +60,16 @@ export const Word = forwardRef<HTMLDivElement, Props>(function Word(
         // the wordIndex is based from array of generated word
         // if the user isn't currently typing this word, make it transparent.
         if (wordIndex > currentWordIndex) {
-          className = "text-gray-400";
+          className = "text-opacity-40";
         }
 
         return (
           <span
             key={j}
             data-letterindex={j}
-            className={"relative inline-block px-[0.8px] " + className}
+            className={
+              "relative inline-block text-[#cad3f0] px-[0.8px] " + className
+            }
           >
             {isCurrent && caretIndex === j && (
               <span className="h-7 w-0.5 rounded-lg absolute left-0 translate-y-[0.5rem] font-bold animate-fade-blink bg-red-700" />
