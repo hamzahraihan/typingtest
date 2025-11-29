00e373905094205003b51181f77e14c3ee474df9
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-11-29 12:34:09 +0700

    Show result component when test is finished
    Added conditional rendering to display the Result component when the typing test state is 'FINISHED'. Also updated grid container to use full screen width and refactored context usage for clarity.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import { TypingTest } from "./components/typing-test.tsx";
 import { useWordContext } from "./context/word-store-context.ts";
 import { difficulties, type Difficulty } from "./utils/difficulty.ts";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Result from "./components/result.tsx";
 
 function App() {
   const inputRef = useRef<HTMLInputElement | null>(null);
-  const { setDifficulty } = useWordContext((state) => state);
+  const setDifficulty = useWordContext((state) => state.setDifficulty);
+  const state = useWordContext((state) => state.state);
 
   return (
     <section className="flex text-[#cad3f0] justify-center bg-[#1f1e2e]">
-      <div className="grid grid-rows-6 p-5 min-h-screen max-w-screen-lg">
+      <div className="grid grid-rows-6 p-5 min-h-screen max-w-screen-lg w-screen">
         <div className="flex flex-col row-span-2 items-start">
           <h1 className="text-3xl font-medium self-center">Typing Test</h1>
           <div className="flex gap-2 items-start mt-auto">
@@ -27,7 +29,11 @@ function App() {
           </div>
         </div>
         <div className="row-span-3 self-center">
-          <TypingTest inputRef={inputRef} />
+          {state === "FINISHED" ? (
+            <Result className="w-full" />
+          ) : (
+            <TypingTest inputRef={inputRef} />
+          )}
         </div>
         <footer className="flex flex-col">
           <div className=" flex mt-auto">
