7ca2a5d0ed7f0f5d10209c68da66f56895a4e1d7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-12-03 08:11:45 +0700

    Refactor result component to use Reset utility
    Replaces manual reload logic in the Result component with the new Reset utility for improved code reuse and maintainability.

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,32 +1,12 @@
 import type { ReactNode } from "react";
 import { useWordContext } from "../context/word-store-context.ts";
-import { RandomWords } from "../utils/random-words.ts";
-import { Timer } from "../utils/timer.ts";
 import { ReloadIcon } from "@radix-ui/react-icons";
+import Reset from "../utils/reset.tsx";
 
 export default function Result({ className }: { className: string }) {
   const { wpm, raw, acc } = useWordContext((state) => state.result);
 
-  const setInputWord = useWordContext((state) => state.setInputWord);
-  const setCurrentWordIndex = useWordContext(
-    (state) => state.setCurrentWordIndex,
-  );
-  const setTypedWords = useWordContext((state) => state.setTypedWords);
-  const setResult = useWordContext((state) => state.setResult);
-  const setState = useWordContext((state) => state.setState);
-  const setRandomWords = useWordContext((state) => state.setRandomWords);
-
-  const timer = Timer.instance;
-
-  const handleReloadTest = () => {
-    setRandomWords(RandomWords.getRandomWords(30).join(" ").split(" "));
-    setInputWord("");
-    setCurrentWordIndex(0);
-    setTypedWords([]);
-    setState("IDLE");
-    timer?.reset();
-    setResult({ wpm: 0, raw: 0, acc: 0 });
-  };
+  const reset = Reset();
 
   return (
     <div className={"flex flex-col gap-2 " + className}>
@@ -35,7 +15,7 @@ export default function Result({ className }: { className: string }) {
       <Status>Acc: {acc.toFixed(0)}%</Status>
       <button
         className="text-sm hover:bg-opacity-50 rounded-lg p-1 bg-[#313244] flex justify-center items-center gap-1 w-fit duration-75"
-        onClick={handleReloadTest}
+        onClick={reset}
       >
         <span className="rounded-lg bg-[#45475a] p-1">
           <ReloadIcon />
